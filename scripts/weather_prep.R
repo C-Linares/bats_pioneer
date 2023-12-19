86ba6c88347b071a5fc510c5be1fb2ee3c0d3c15
CarlosLinares <55813567+C-Linares@users.noreply.github.com>
2023-12-19 08:33:06 -0700

    working on weather

--- scripts/weather_prep.R
+++ scripts/weather_prep.R
@@ -11,16 +11,36 @@
 library(tidyverse)
 library(magrittr)
 library(data.table)
+library(purrr)
 
 
 
-#----------- load data
+
+#----------- load data ---------------
 wetfls<-list.files('data_for_analysis/weather/',pattern = "*.csv",full.names = T) # list csvs
 
+# this creates the data frame list but not the data base. 
 wetdb <- lapply(wetfls, function(wetfls) {
   read.csv(wetfls, skip =  7) # Skips 7 rows (header + 4 data rows)
 })
 
-library(purrr)
 
-wetdb <- purrr::map_df(wetfls, ~ read.csv(.x, skip = 7))
+wetdb <- purrr::map_df(wetfls, ~ read.csv(.x, skip = 7)) # new function from purr to read multiple files 
+# data cleaning --------------
+
+wetdb<- setnames(wetdb, old = "X.1", new = "date.time") # change col name to date
+
+wetdb$date.time<- mdy_hms(wetdb$date.time)
+
+wetdb$wk<- week(wetdb$date.time)
+
+wetdb <- wetdb %>% select(-"Millimeters")# remove empty colum
+
+# -- calculate the mean temp and rain -----
+
+wtemp <- wetdb %>%
+  group_by(wk) %>%
+  summarize(mean_tem = mean(Celsius), mean_wind = mean(m.s))# there are NA's I don't know from where.
+
+# Filter and save rows with NAs
+na_rows <- wetdb[!complete.cases(wetdb), ]
