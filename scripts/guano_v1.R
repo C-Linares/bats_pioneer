e6fbc4b7a695a251f772a847b17ea1ddf2d8c17c
C-Linares <carlosgarcialina@u.boisestate.edu>
2025-02-04 17:18:20 -0700

    some guano code

--- scripts/guano_v1.R
+++ scripts/guano_v1.R
@@ -60,3 +60,53 @@ batr::read.guano('data_for_analysis/sample_wavs/IRON01_20210729_232359-Mylu.wav'
 GUANO_reader(pp, "pioneer") # function to be retired soon. # this worked
 import_GUANO("New","data_for_analysis/sample_wavs/", 
              "C:/Users/Carlos/Documents/R/bats_pioneer/data_for_analysis/sample_wavs/batr_out")
+
+
+# guano from python
+
+
+library(reticulate)
+use_python("C:/Users/Carlos/AppData/Local/Programs/Python/Python38/python.exe")  # Specify your Python path if needed
+guano <- import("guano")
+
+# Load a .WAV file with GUANO metadata
+g <- guano$GuanoFile('C:/Users/Carlos/Documents/R/bats_pioneer/data_for_analysis/sample_wavs/IRON01_20210729_232359-Mylu.wav')
+
+# Access metadata
+version <- g['GUANO|Version']
+make <- g['Make']
+model <- g['Model']
+
+print(version)
+print(make)
+print(model)
+
+
+# another try at guano function to work. 
+# 
+library(guano)
+dt1 <- read.guano.dir(dirname = 'data_for_analysis/sample_wavs/' ,
+                      pattern = "*.wav" ,
+                      recursive = T) # this code from guano in R worked.
+
+# now I want to see if the files that have been ID with kp and sono bat show or if running Kp erase sonobat. 
+
+# I ran the same files but with kaleidoscope to see if the process changed the metadata. 
+dt2 <- read.guano.dir(dirname = 'data_for_analysis/sample_wavs/',
+                      recursive = F)
+a<-read.guano()
+
+
+# now let's try with the bioacustics package
+# the following code work but does not provide an easy way to save the metadata into R. 
+# I also learnt that 
+
+
+
+install.packages("bioacoustics")
+
+library(bioacoustics)
+t<-read_audio('data_for_analysis/sample_wavs/IRON01_20210729_232359-Mylu.wav')
+class(t)
+a<-metadata("data_for_analysis/sample_wavs/IRON01_20210729_232359-Mylu.wav")
+a<-metadata(t)
