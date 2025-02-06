dbee577078223101c92295dda8baf9ab5e9e596f
C-Linares <carlosgarcialina@u.boisestate.edu>
2025-02-05 17:02:38 -0700

    guano and weather

---
+++ scripts/guano_data_compile.R
@@ -0,0 +1,24 @@
+# Load required libraries
+library(future)
+library(future.apply)
+library(guano)
+
+# Set up a plan for asynchronous processing
+plan(multisession)
+
+# Define the function to be run in the background
+process_files <- function() {
+  execution_time <- system.time({
+    dt2 <- read.guano.dir(dirname = 'Z:/PioneerLights_2021/', recursive = T)
+  })
+  return(execution_time)
+}
+
+# Run the function as a background job
+future_result <- future({
+  process_files()
+})
+
+# Print the execution time once the job is complete
+execution_time <- value(future_result)
+print(execution_time)
--- scripts/guano_v1.R
+++ scripts/guano_v1.R
@@ -46,8 +46,9 @@ dt1 <- read.guano.dir(dirname = 'data_for_analysis/sample_wavs/' ,
 # now I want to see if the files that have been ID with kp and sono bat show or if running Kp erase sonobat. 
 
 # I ran the same files but with kaleidoscope to see if the process changed the metadata. 
+
 execution_time <- system.time({  #processing 882 files took 83 secs. Maybe I can do all of them...
-  dt2 <- read.guano.dir(dirname = 'Z:/PioneerLights_2021/iron01/sm4/Data', recursive = FALSE)
+  dt2 <- read.guano.dir(dirname = 'Z:/PioneerLights_2021/', recursive = FALSE)
 })
 
 # Print the execution time
